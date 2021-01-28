81e02916f116f891526f1740a6fafe0c3bfe7468
vandana <vandy.senthil@gmail.com>
2021-01-28 11:58:40 -0500

    added repo to amplify

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 /*import 'bootstrap/dist/css/bootstrap.min.css';*/
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 
 
